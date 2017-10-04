da88941e017c6499c0376896662d64ca7dfaa84a
Joe <jsbenning4@gmail.com>
2017-10-04 07:34:49 -0400

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,24 @@
 // Code Invitation Component Here
+import React from 'react';
+
+
+class Invitation extends React.Component {
+  render() {
+    
+    const myHeading = "You've been invited!";
+    const myChildren = React.Children.map(this.props.children, child => {
+      return (
+          <div>{child}</div>
+        );
+    });
+
+    return (
+      <div className="myInvitation">
+        <h1>{myHeading}</h1>
+        {myChildren}
+      </div>
+    );
+  }
+}
+
+export default Invitation;
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,21 @@
 // Code ThemedDecoration Component Here
+import React from 'react';
+
+class ThemedParty extends React.Component {
+  render() {
+    const theme = this.props.theme;
+    const myChildren = React.Children.map(this.props.children, child => {
+      return (
+        <div className={theme}>{child}</div>
+        );
+      });
+
+      return (
+      <div>
+      {myChildren}
+      </div>
+    );
+  } 
+}
+export default ThemedParty
+
